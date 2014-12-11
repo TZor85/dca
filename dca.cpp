6f08df52cb988088fd809701df6924789b992c54
Alberto Cervantes <amcb9@alu.ua.es>
2014-12-11 19:20:54 +0100

    Permite introducir el primer numero

---
+++ dca.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1,num2,resultado;
+
+    cout << "Introduzca un numero entero: " << endl;
+    cin >> num1;
+    cout << endl;
+
+
+
+    return 0;
+
+}
